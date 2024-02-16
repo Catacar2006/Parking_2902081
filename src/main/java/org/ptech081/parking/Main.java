604406e458de1d360f7bbe07a2c86c6f11542131
unknown <Aprendiz@BOGDFPCGMS4142.FormacionSena.RED>
2024-02-16 10:20:50 -0500

    registros de E/S completos

--- src/main/java/org/ptech081/parking/Main.java
+++ src/main/java/org/ptech081/parking/Main.java
@@ -1,12 +1,19 @@
 package org.ptech081.parking;
 
 import org.ptech081.parking.entities.*;
+import java.time.LocalTime;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
+import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
 
         //crear carros con los constructores: 
-        Vehiculo carrito1 =new Vehiculo("GHI 89J", TipoVehiculo.MOTO);
+        Vehiculo carrito1 =new 
+        Vehiculo("GHI 89J", 
+        TipoVehiculo.MOTO);
         
         Vehiculo carrito2 = new Vehiculo();
         carrito2.placa = "JSD 283";
@@ -14,16 +21,58 @@ public class Main {
 
         //crear clientes 
         Cliente cliente1 = new Cliente("Karol", "Cardona", 578942145L);
+        Cliente cliente2 = new Cliente("David", "Lopez", 578945L);
 
         //invocar el metodo addVehiculo
         cliente1.addVehiculo(carrito1);
         cliente1.addVehiculo("TYG 546", TipoVehiculo.SEDAN);
 
-        //recorrer los vehiculos de los clientes 
-        for( Vehiculo v: cliente1.misCarros){
-            System.out.println(v.placa + "" + v.tipoVehiculo);
-            System.out.println( "---------------" );
+        //invocar el metodo addVehiculo
+        cliente2.addVehiculo(carrito2);
+        cliente2.addVehiculo("HGT 023", TipoVehiculo.BUS);
+
+        // Crear dos cupos 
+        Cupo cupo1 = new Cupo('A');
+        Cupo cupo2 = new Cupo('B');
+
+        // Crar dos registros  E/S
+        LocalDate fechaInicio = LocalDate.now();
+        LocalTime horaInicio = LocalTime.now();
+        LocalDate fechaFin = LocalDate.of(2024, Month.APRIL, 27);
+        LocalTime horaFin = LocalTime.of(18, 30, 15);
+
+
+        //guardar los registros en una lista
+        List<Registro>misRegistros =  new ArrayList<Registro>();
+
+        Registro registro1 = new  Registro(fechaInicio, horaInicio, fechaFin, horaFin, 10000L, cupo1, cliente1.misCarros.get(1), cliente1);
+        misRegistros.add(registro1);
+
+
+        //registro nuevo 2
+
+        // Crar dos registros  E/S
+        LocalDate fechaInicio2 = LocalDate.now();
+        LocalTime horaInicio2 = LocalTime.now();
+        LocalDate fechaFin2 = LocalDate.of(2024, Month.APRIL, 25);
+        LocalTime horaFin2= LocalTime.of(23, 45, 15);
+
+        //crear registro 
+        Registro registro2 = new Registro (fechaInicio2, horaInicio2, fechaFin2, horaFin2, 25000L, cupo2, cliente2.misCarros.get(1), cliente2);
+                misRegistros.add(registro2);
+
+
+
+        System.out.println("Registros de E/S parking");
+        for(Registro r: misRegistros){
+            System.out.println("Cliente" + r.cliente.nombre + " " + r.cliente.apellidos + "|" + r.vehiculo.placa +" " 
+                                            + r.vehiculo.tipoVehiculo +"|" + r.valor + "|" + r.fechaInicio +"|" + r.horaInicio +"|" + r.fechaFin +"|" + r.horaFin +"|");
         }
+
+        
+
+        
+       
 
     }
 }
--- src/main/java/org/ptech081/parking/entities/Registro.java
+++ src/main/java/org/ptech081/parking/entities/Registro.java
@@ -1,5 +1,41 @@
 package org.ptech081.parking.entities;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class Registro {
+    // Atributos intrinsecos
+    public LocalDate fechaInicio;
+    public LocalTime horaInicio;
+    public LocalDate fechaFin;
+    public LocalTime horaFin;
+    public Long valor;
+
+    // Atributos de relacion
+
+    public Cupo cupo;
+    public Vehiculo vehiculo;
+    public Cliente cliente;
+    
+
+    //Constructores 
+    public Registro() {
+        
+    }
+
+
+    public Registro(LocalDate fechaInicio, LocalTime horaInicio, LocalDate fechaFin, LocalTime horaFin, Long valor,
+            Cupo cupo, Vehiculo vehiculo, Cliente cliente) {
+        this.fechaInicio = fechaInicio;
+        this.horaInicio = horaInicio;
+        this.fechaFin = fechaFin;
+        this.horaFin = horaFin;
+        this.valor = valor;
+        this.cupo = cupo;
+        this.vehiculo = vehiculo;
+        this.cliente = cliente;
+    }
+
+
+
 
 }
